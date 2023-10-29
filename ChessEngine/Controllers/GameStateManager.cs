da84b196843c1368a875e09cc050f6d7f6995beb
moAnmol <bartiemus21@gmail.com>
2023-10-29 17:47:40 +0000

    Legal moves

--- ChessEngine/Controllers/GameStateManager.cs
+++ ChessEngine/Controllers/GameStateManager.cs
@@ -1,6 +1,51 @@
+using ChessEngine;
+
 namespace Utility;
+
 
 public class GameStateManager
 {
+    public List<ChessPiece> pieces = new List<ChessPiece>();
+    private TurnToMove toMove;
+    private bool whiteToMove = true;
+    
+    public static GameStateManager Instance { get; private set; }
+    public TurnToMove GetTurnToMove => toMove;
+    public void SetTurnToMove(TurnToMove move)
+    { 
+        toMove = move;
+    }
+    public GameStateManager()
+    {
+        if(Instance == null) Instance = new GameStateManager();
+    }
+    
+    //Entry point
+    public void UpdateTurns()
+    {
+        ChangeTurns();
+        //Update for later and send validation string for turn update alongside move validation
+    }
+
+    private bool ChangeTurns()
+    { 
+        whiteToMove = !whiteToMove;
+        return whiteToMove;
+    }
+
+
+
+
+
+
+
+}
+
+
+
+public enum TurnToMove
+{
+    black ,
+    white
     
 }
--- ChessEngine/LegalMoves.cs
+++ ChessEngine/LegalMoves.cs
@@ -1,6 +1,74 @@
 namespace ChessEngine;
 
-public class LegalMoves
-{
+
+// This is a set in stone , defined file . 
+
+sealed class LegalMoves
+{   
     
+    // need to run like a time check on how long this takes to run and-
+    // further optimize it
+    private void CheckForMoves(int[] board)
+    {
+
+        for (int i = 0; i < board.Length; i++)
+        {   //i -> index
+            int pieceCode = 00; // extra piece code and colour code from here
+            int colorCode;
+
+            if (pieceCode == Piece.Pawn)
+            {
+                GenerateMovesForPawn(i, pieceCode , board);
+            }
+        }
+
+    }
+
+    //going thru every peice
+    private ChessPiece GenerateMovesForPawn(int ind, int p , int[] board)
+    {   
+        int index = ind;
+        int pCode = p;
+        int colorCode;
+        Pawn pawn = new Pawn(pCode);
+        
+        //Start testing for every possible moves without any problems .
+        //only discount invalid moves , NOT ILLEGAL MOVES . 
+        
+        //front move -> normal move
+        if(board[index + pawn.pawnStep] == Piece.Empty)
+            pawn.SetAllPossibleMoves(index+ pawn.pawnStep);
+        
+        //RIGHT MOVE
+        //if(board[index + pawn.pawnStep +1 ] == (Piece.Black | ))
+            
+            
+        return pawn;
+        
+    }
+
+}
+
+public class Pawn : ChessPiece
+{
+    public  readonly int pawnStep = 8;
+    public Pawn(int pCode )
+    {
+        this.pieceCode = pCode;
+    }
+    public int GetPieceCode => pieceCode;
+    public void SetAllPossibleMoves(int index)
+    {
+        allPossibleMovesIndex.Add(index);
+        
+    }
+}
+
+
+public abstract class ChessPiece
+{   
+    protected int pieceCode { get; init; }
+    protected HashSet<int> allPossibleMovesIndex = new HashSet<int>();
+    
+
 }
