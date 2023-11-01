c00fba04951c01672deb2cd596a5ea217c1b4181
moAnmol <bartiemus21@gmail.com>
2023-11-01 12:48:20 +0000

    Small changes , capture mechanic mostly reworked from UI side

--- ChessEngine/Board.cs
+++ ChessEngine/Board.cs
@@ -38,15 +38,15 @@ namespace ChessEngine
     
     public bool MakeMove(int piece, int oldIndex, int newIndex)
     {   
-        Console.WriteLine($"piece {piece} and old index {oldIndex} and new index {newIndex}");
+       // Console.WriteLine($"piece {piece} and old index {oldIndex} and new index {newIndex}");
         foreach (var p in GameStateManager.Instance.allPiecesThatCanMove)
         {
             
             if (p.GetPieceCode == piece && p.GetAllMovesForThisPiece.Contains(newIndex) &&
                 oldIndex == p.GetCurrentIndex)
             {
-                Console.WriteLine("Piece that moved" + piece + "with old index -> " + oldIndex + " and new index ->" +
-                                  newIndex);
+               // Console.WriteLine("Piece that moved" + piece + "with old index -> " + oldIndex + " and new index ->" +newIndex);
+                CheckForBonusBasedOnPieceCapture(piece,chessBoard[newIndex]);
                 chessBoard[oldIndex] = Piece.Empty;
                 chessBoard[newIndex] = piece;
                 ShowBoard();
@@ -62,7 +62,44 @@ namespace ChessEngine
             return false;
      }
 
-   
+    private void CheckForBonusBasedOnPieceCapture(int pieceThatMoved, int newIndex)
+    
+    {
+        int pCode =newIndex & Piece.CPiece;
+        Console.WriteLine("PCode is that got captured" + pCode);
+        Console.WriteLine("Chessboard Index" + newIndex);
+        
+        switch (pCode)
+        {
+            case 0: 
+                Console.WriteLine("0 point for Empty");
+                break;
+            case 1: 
+                Console.WriteLine("1 point for Pawn");
+                break;
+            case 2:
+                Console.WriteLine("2 points for Rook");
+                break;
+            case 3: 
+                Console.WriteLine("3 points for Knight");
+                break;
+            case 4: 
+                Console.WriteLine("4 points for Bishop");
+                break;
+            case 6: Console.WriteLine("6 points for Queen");
+                break;
+            
+            
+        }
+    }
+
+
+    public void CheckForBonusBasedOnPieceCapture()
+    {
+        
+    }
+
+
 
     private void ShowBoard()
      {
