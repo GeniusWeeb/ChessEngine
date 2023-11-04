354d40d2ee8acdb8630f2b001c19a45c595ad31b
moAnmol <bartiemus21@gmail.com>
2023-11-04 20:14:45 +0000

    minor fixed

--- ChessConsoleApp/Program.cs
+++ ChessConsoleApp/Program.cs
@@ -13,13 +13,14 @@ public class Program
     {
         
          ChessEngineSystem.Instance.Init();
-         Connection.Instance.Init(); // this will also creat the static instance of the class on the go
+         // this will also creat the static instance of the class on the go
          // We dont need to do  ,  Connection conn =  new Connection();
         
          AppDomain.CurrentDomain.ProcessExit += (sender, args) =>
          { 
              ChessEngineSystem.Instance.Dispose();
          };
+
          
          Console.ReadLine();
          
